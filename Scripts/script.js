03b57e18118843301a71e29308886822c8fa6aa8
Leandro <leandrosantos020788@gmail.com>
2020-09-22 15:47:16 -0300

    Update

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,17 +1,15 @@
-const modalOverley = document.querySelector('.modal-overlay');
+const modalOverley = document.querySelector(".modal-overlay");
 
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll(".card");
 
 for (let card of cards) {
-  card.addEventListener("click", function() {
-
-    modalOverley.classList.add('active')
-  
-  })
+  card.addEventListener("click", function () {
+    modalOverley.classList.add("active");
+    modalOverley.querySelector("iframe").src =
+      "https://leandrohsantos.com.br/images/dog.png";
+  });
 }
 
- document.querySelector(".close-modal").addEventListener("click", function() {
-   modalOverley.classList.remove("active")
- })
-
- 
+document.querySelector(".close-modal").addEventListener("click", function () {
+  modalOverley.classList.remove("active");
+});
