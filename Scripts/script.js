e5d25b18b19054f0cd77c12ac3187c437b48486b
Leandro <leandrosantos020788@gmail.com>
2020-09-23 10:56:51 -0300

    Update

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,20 +1,18 @@
-const modalOverley = document.querySelector('.modal-overlay');
+const modalOverley = document.querySelector(".modal-overlay");
 
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 for (let card of cards) {
   card.addEventListener("click", function () {
-    const imageId = card.getAttribute("id");
+    const videoId = card.getAttribute("id");
 
     modalOverley.classList.add("active");
     modalOverley.querySelector(
-      "img"
-    ).src = `https://leandrohsantos.com.br/images/${imageId}`;
+      "iframe"
+    ).src = `https://www.youtube.com/embed/${videoId}`;
   });
 }
 
 document.querySelector(".close-modal").addEventListener("click", function () {
-  modalOverley.classList.remove("active")
-  modalOverley.querySelector("img").src = ""
-  
+  modalOverley.classList.remove("active");
 });
