076d4ef51d4163a4e48c7d1c6b643df934f95b23
Leandro <leandrosantos020788@gmail.com>
2020-09-24 11:10:24 -0300

    Server update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const app = express()
+
+app.use(express.static('public'))
+
+app.set("view engine", "html")
+
+nunjucks.configure("views", {
+  express:app
+})
+app.get("/", function(req, res){
+
+  return res.render("index")
+})
+
+app.get("/classes", function(req, res){
+
+  return res.render("classes")
+})
+
+app.listen(5000, function() {
+  console.log(" server in running ")
+})
