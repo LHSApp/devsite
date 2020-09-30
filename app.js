0500ab6184eb1018d819e169feaefb97538c0d40
Leandro <leandrosantos020788@gmail.com>
2020-09-29 22:42:21 -0300

    Update

--- app.js
+++ app.js
@@ -8,11 +8,24 @@ app.use(express.static('public'))
 app.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express:app
+  express:app,
+  autoescape: false
 })
 app.get("/", function(req, res){
 
-  return res.render("about")
+  const about = {
+  avatar_url: "https://avatars0.githubusercontent.com/u/48134318?s=460&u=1ab8aefde787685c5913a792411c4c6f4d17aba8&v=4",
+  name: "Leandro Santos ",
+  role: "Programador Web",
+  link: [
+    {name: "Github", url:" https://github.com/LHSApp"},
+    {name: "Instagram", url:" https://github.com/LHSApp"},
+    {name: "Contato", url:" https://github.com/LHSApp"},
+  ]
+
+  }
+
+  return res.render("about", {about})
 })
 
 app.get("/classes", function(req, res){
