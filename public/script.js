a9efc259fef76de388daf1b2361e5ac1ba11165c
Leandro <leandrosantos020788@gmail.com>
2020-10-02 09:35:12 -0300

    Update

--- public/script.js
+++ public/script.js
@@ -5,15 +5,9 @@ const cards = document.querySelectorAll(".card");
 for (let card of cards) {
   card.addEventListener("click", function () {
     const videoId = card.getAttribute("id");
+   window.location.href = `/video?id=${videoId}`
 
-    modalOverley.classList.add("active");
-    modalOverley.querySelector(
-      "iframe"
-    ).src = `https://www.youtube.com/embed/${videoId}`;
+
   });
 }
 
-document.querySelector(".close-modal").addEventListener("click", function () {
-  modalOverley.classList.remove("active");
-  modalOverley.querySelector("iframe").src = " "
-});
