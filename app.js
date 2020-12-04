48541e206e5c381ac07c115bfff3a664bfbb76dd
Leandro <leandrosantos020788@gmail.com>
2020-12-04 17:38:30 -0300

    Update site

--- app.js
+++ app.js
@@ -29,16 +29,16 @@ app.get("/", function(req, res){
   return res.render("about", {about})
 })
 
-app.get("/App", function(req, res){
+app.get("/Portfolio", function(req, res){
 
   const about = {
   avatar_url: "https://avatars0.githubusercontent.com/u/48134318?s=460&u=1ab8aefde787685c5913a792411c4c6f4d17aba8&v=4",
-  name: "Leandro Santos ",
-  role: "Programador Web",
+  name: "LHSApp ",
+  role: "",
   link: [
-    {name: "Github", url:" https://github.com/LHSApp"},
-    {name: "Instagram", url:" https://www.instagram.com/leandro.h.santos/"},
-    {name: "Contato", url:" mailto:leandro@leandrohsantos.com.br"},
+    {name: "CrazyBird", url:"https://play.google.com/store/apps/details?id=com.leandro.jogo&hl=pt_BR"},
+    {name: "Flying", url:" https://play.google.com/store/apps/details?id=not.yet.specified.leandro&hl=pt_BR"},
+    {name: "Ecoleta", url:"https://leandrohsantos.online/"},
   ]
 
   }
